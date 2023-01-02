0e1f52398a836c2c1c3c761abcda3ab301c95718
Krishna1081 <90314846+Krishna1081@users.noreply.github.com>
2023-01-02 23:54:33 +0530

    Day-02 Primitve Data Types #2

--- Day-2/primitiveDataTypes.py
+++ Day-2/primitiveDataTypes.py
@@ -1,4 +1,23 @@
 # Data Types
 
 # String
-print("Hello"[4])
+print("")
+print("Hello"[4]) #slice 
+print("123" + "345")
+
+# Integer
+print("")
+print("Integer")
+print(123+345)
+print(123_345_567)
+
+# Float
+print("")
+print("Float")
+print(3.14)
+
+# Boolean
+print("")
+print("Boolean")
+print(True)
+print(False)
